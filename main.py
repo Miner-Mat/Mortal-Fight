a4ada0e196ed6b829c64540c5edf7b9b544c900a
User <minermat007@gmail.com>
2023-11-28 12:03:05 +0700

    First version commit

--- main.py
+++ main.py
@@ -2,43 +2,50 @@ import pygame  # импортируем pygame
 
 # Инициализация Pygame
 pygame.init()
-screen = pygame.display.set_mode((900, 515))
-pygame.display.set_caption("Mortal Fight")
-icon = pygame.image.load("logo.jpg")
-pygame.display.set_icon(icon)
+screen = pygame.display.set_mode((1920, 1080))  # Задаём разрешение основного окна
+pygame.display.set_caption("Mortal Fight")  # Задаём название программе
+icon = pygame.image.load("logo.jpg")  # Загружаем логотип
+pygame.display.set_icon(icon)  # Выставляем логотип
 
-pygame.mixer.init()
-pygame.mixer.music.load("music.mp3")
-pygame.mixer.music.set_volume(0.5)
-pygame.mixer.music.play(-1)
+pygame.mixer.init()  # инициализируем функцию добавления музыки
+pygame.mixer.music.load("music.mp3")  # Загружаем музыку
+pygame.mixer.music.set_volume(0.5)  # Выставляем громкость
+pygame.mixer.music.play(-1)  # Запускаем бесконечный цикл проигрывания
 
-# Определяющие положение персонажа координаты
-x, y = -10, 110
+# Определяющие положение персонажа переменные
+x, y = 160, 710
 early_x = -120
-final_x = 700
+final_x = 1720
 speed = 5
+
+# Определяющие положение второго персонажа переменные
+x2, y2 = 1580, 710
+early_x2 = -120
+final_x2 = 1720
+speed2 = 5
 
 # Загружаем изображения
 bg = pygame.image.load("location.jpg")
-player = pygame.image.load("Character1/character.png")
 
-anim_images = [pygame.image.load("Character1/-run10.png"), pygame.image.load("Character1/-run9.png"),
-               pygame.image.load("Character1/-run8.png"), pygame.image.load("Character1/-run7.png"),
-               pygame.image.load("Character1/-run6.png"), pygame.image.load("Character1/-run5.png"),
-               pygame.image.load("Character1/-run4.png"),
-               pygame.image.load("Character1/-run3.png"),
-               pygame.image.load("Character1/-run2.png"),
-               pygame.image.load("Character1/-run1.png"), pygame.image.load("Character1/-character.png"),
-               pygame.image.load("Character1/character.png"),
-               pygame.image.load("Character1/run1.png"), pygame.image.load("Character1/run2.png"),
-               pygame.image.load("Character1/run3.png"), pygame.image.load("Character1/run4.png"),
-               pygame.image.load("Character1/run5.png"), pygame.image.load("Character1/run6.png"),
-               pygame.image.load("Character1/run7.png"), pygame.image.load("Character1/run8.png"),
-               pygame.image.load("Character1/run9.png"), pygame.image.load("Character1/run10.png")]
-current_frame = 3
+anim_st = [pygame.image.load("Character_st/1st.png"), pygame.image.load("Character_st/2st.png"),
+           pygame.image.load("Character_st/3st.png"), pygame.image.load("Character_st/4st.png"),
+           pygame.image.load("Character_st/5st.png"), pygame.image.load("Character_st/6st.png"),
+           pygame.image.load("Character_st/7st.png")]
+
+anim_run = [pygame.image.load("charact_run/run1.png"), pygame.image.load("charact_run/run2.png"),
+            pygame.image.load("charact_run/run3.png"), pygame.image.load("charact_run/run4.png"),
+            pygame.image.load("charact_run/run5.png"), pygame.image.load("charact_run/run6.png"),
+            pygame.image.load("charact_run/run7.png"), pygame.image.load("charact_run/run8.png")]
+
+current_frame = 0  # текущий кадр
+current_frame_run = 0  # последний обновлённый кадр бега персонажа
 animation_delay = 100  # Задержка между кадрами
-last_update = pygame.time.get_ticks()
+last_update = pygame.time.get_ticks()  # последний обновлённый кадр
 
+current_frame2 = 0
+current_frame_run2 = 0
+
+# Загружаем изображения первого и второго хэлфбара
 health_bar_1 = pygame.image.load("health_bar.png")
 health_bar_2 = pygame.image.load("health_bar.png")
 
@@ -52,50 +59,59 @@ standing = True
 left = False
 right = True
 
+standing2 = True
+left2 = False
+right2 = True
+
+count = 6
+
 running = True  # флаг работы
 while running:
     clock.tick(60)  # обновление экрана 60 раз в секунду
 
-    # Привязываем кнопки для передвижения
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
         if x - speed < early_x:
             x = early_x
-            if current_frame == 0:
-                current_frame = 9
-            current_frame -= 1
+            if current_frame_run == 0:
+                current_frame_run = 7
+            current_frame_run -= 1
         else:
             x -= speed
-            if current_frame == 0:
-                current_frame = 9
-            current_frame -= 1
+            if current_frame_run == 0:
+                current_frame_run = 7
+            current_frame_run -= 1
         standing = False
         right = False
         left = True
-    if keys[pygame.K_RIGHT]:
+
+    if keys[pygame.K_d]:
         if x + speed > final_x:
             x = final_x
-            if current_frame == 21:
-                current_frame = 12
-            current_frame += 1
+            if current_frame_run == 7:
+                current_frame_run = 0
+            current_frame_run += 1
         else:
             x += speed
-            if current_frame == 21:
-                current_frame = 12
-            current_frame += 1
+            if current_frame_run == 7:
+                current_frame_run = 0
+            current_frame_run += 1
         standing = False
         right = True
         left = False
 
-    # Отображаем элементы игры на экране
     screen.blit(bg, (0, 0))
-    if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
-        if left:
-            screen.blit(pygame.image.load("Character1/-character.png"), (x, y))
-        elif right:
-            screen.blit(player, (x, y))
-    else:
-        screen.blit(anim_images[current_frame], (x, y))
+    if not keys[pygame.K_a] and not keys[pygame.K_d]:
+        screen.blit(pygame.transform.scale(anim_st[current_frame], (170, 300)), (x, y))
+    elif keys[pygame.K_a]:
+        screen.blit(pygame.transform.flip(pygame.transform.scale(anim_run[current_frame], (290, 300)), True, False), (x, y))
+    elif keys[pygame.K_d]:
+        screen.blit(pygame.transform.scale(anim_run[current_frame], (290, 300)),(x, y))
+    if current_frame == 6:
+        current_frame = 0
+    elif count % 6 == 0:
+        current_frame += 1
+    count += 1
     pygame.display.update()
 
     # Скрипт выхода из игры
@@ -105,7 +121,7 @@ while running:
             pygame.quit()
 
     # Отрисовка хэлф баров
-    pygame.draw.rect(bg, (0, 255, 0), (18, 20, current_health_1 * 3, 20))
-    pygame.draw.rect(bg, (255, 255, 255), (18, 20, current_health_1 * 3, 20), width=2)
-    pygame.draw.rect(bg, (0, 255, 0), (580, 20, current_health_1 * 3, 20))
-    pygame.draw.rect(bg, (255, 255, 255), (580, 20, current_health_1 * 3, 20), width=2)
+    pygame.draw.rect(bg, (0, 255, 0), (38, 20, current_health_1 * 3, 40))
+    pygame.draw.rect(bg, (255, 255, 255), (38, 20, current_health_1 * 3, 40), width=2)
+    pygame.draw.rect(bg, (0, 255, 0), (1560, 20, current_health_1 * 3, 40))
+    pygame.draw.rect(bg, (255, 255, 255), (1560, 20, current_health_1 * 3, 40), width=2)
