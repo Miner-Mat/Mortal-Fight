98d9bd689151772e3dce4583cdd1f63a395a4c38
User <minermat007@gmail.com>
2023-11-30 12:20:22 +0700

    First version commit

--- main.py
+++ main.py
@@ -27,20 +27,24 @@ speed2 = 5
 # Загружаем изображения
 bg = pygame.image.load("location.jpg").convert_alpha()
 
+# Анимации персонажа в момент неподвижности вправо
 anim_st = [pygame.image.load("Character_st/1st.png"), pygame.image.load("Character_st/2st.png"),
            pygame.image.load("Character_st/3st.png"), pygame.image.load("Character_st/4st.png"),
            pygame.image.load("Character_st/5st.png"), pygame.image.load("Character_st/6st.png"),
            pygame.image.load("Character_st/7st.png")]
 
+# Анимации персонажа в момент неподвижности влево
 minus_anim_st = [pygame.image.load("-Character_st/1st.png"), pygame.image.load("-Character_st/2st.png"),
                  pygame.image.load("-Character_st/3st.png"), pygame.image.load("-Character_st/4st.png"),
                  pygame.image.load("-Character_st/5st.png"), pygame.image.load("-Character_st/6st.png"),
                  pygame.image.load("-Character_st/7st.png")]
 
+# Анимации персонажа во время атаки
 anim_fight = [pygame.image.load("Character_fight/Attack_1.png"), pygame.image.load("Character_fight/Attack_2.png"),
               pygame.image.load("Character_fight/Attack_3.png"), pygame.image.load("Character_fight/Attack_4.png"),
               pygame.image.load("Character_fight/Attack_5.png")]
 
+# Анимации персонажа во время бега
 anim_run = [pygame.image.load("charact_run/run1.png"), pygame.image.load("charact_run/run2.png"),
             pygame.image.load("charact_run/run3.png"), pygame.image.load("charact_run/run4.png"),
             pygame.image.load("charact_run/run5.png"), pygame.image.load("charact_run/run6.png"),
@@ -82,10 +86,26 @@ def health_bar():  # Отрисовка хэлф баров
     pygame.draw.rect(bg, (0, 255, 0), (1560, 20, current_health_2 * 3, 40))
     pygame.draw.rect(bg, (255, 255, 255), (1560, 20, current_health_2 * 3, 40), width=2)
 
-running = True  # флаг работы
-while running:
-    clock.tick(60)  # обновление экрана 60 раз в секунду
-    health_bar()  # вызываем отрисовку хэлф баров
+
+def frame_check():  # Проверка кадров
+    global current_frame, count, current_frame2, current_frame_fight
+    if current_frame == 6:
+        current_frame = 0
+    elif count % 6 == 0:
+        current_frame += 1
+        current_frame2 += 1
+    if current_frame2 == 6:
+        current_frame2 = 0
+    if current_frame_fight == 4:
+        current_frame_fight = 0
+    elif count % 6 == 0:
+        current_frame_fight += 1
+    count += 1
+
+
+def key_check():  # Проверка нажатий
+    global x, current_frame_run, early_x, early_x2, standing, right, left, speed, final_x
+    global standing2, right2, left2, x2, current_frame_run2, speed2, final_x2, early_x2
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
         if x - speed < early_x:
@@ -147,7 +167,10 @@ while running:
         left2 = False
         right2 = True
 
-    screen.blit(bg, (0, 0))
+
+def key_work():  # Обработка нажатий
+    global current_health_2
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_f]:
         current_health_2 -= 25
 
@@ -178,19 +201,18 @@ while running:
     elif keys[pygame.K_RIGHT]:
         screen.blit(pygame.transform.scale(anim_run[current_frame2], (290, 300)), (x2, y2))
 
-    if current_frame == 6:
-        current_frame = 0
-    elif count % 6 == 0:
-        current_frame += 1
-        current_frame2 += 1
-    if current_frame2 == 6:
-        current_frame2 = 0
-    if current_frame_fight == 4:
-        current_frame_fight = 0
-    elif count % 6 == 0:
-        current_frame_fight += 1
-    count += 1
-    pygame.display.update()
+
+running = True  # флаг работы
+while running:
+    clock.tick(60)  # обновление экрана 60 раз в секунду
+    health_bar()  # вызываем отрисовку хэлф баров
+
+    key_check()  # вызываем проверку нажатий
+    screen.blit(bg, (0, 0))  # отрисовываем фон
+
+    key_work()  # вызываем обработку нажатий
+    frame_check()  # вызываем проверку кадров
+    pygame.display.update()  # обновляем окно
 
     # Скрипт выхода из игры
     for event in pygame.event.get():
